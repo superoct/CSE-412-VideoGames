453fe36036bc22e3e20107db37f017754a9fbc0a
superoct <51865513+superoct@users.noreply.github.com>
2020-11-22 22:29:50 -0700

    Update login.php
    Added the command to add something to the database

--- login.php
+++ login.php
@@ -1,8 +1,14 @@
 <?php
-if(isset($_POST['submitLogin']))
-{
-  $db = new PDO('pgsql:host=database-412.cizjli6h3heh.us-west-2.rds.amazonaws.com;dbname=myDB', 'root', 'videogames');
-  echo "Connected to PostgreSQL";
+$db = new PDO('pgsql:host=database-412.cizjli6h3heh.us-west-2.rds.amazonaws.com;dbname=myDB', 'root', 'videogames');
 
+if(isset($_POST['submitAdd']))
+{
+  try{
+    $query = "INSERT INTO public.\"Game_Title\" VALUES('$_POST[vgName]', '$_POST[vgEngine]', '$_POST[vgFranchise]', '$_POST[vgDescription]', '$_POST[vgID]')";
+    $db->query($query);
+    echo "Added";
+  } catch(PDOException $e){
+    echo $e->getMessage();
+  }
 }
 ?>
