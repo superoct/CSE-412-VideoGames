9136850d9e75512ca96b11d5a3bcabd6743e86b6
superoct <51865513+superoct@users.noreply.github.com>
2020-11-28 15:49:46 -0700

    Update login.php
    Added the PHP code

--- login.php
+++ login.php
@@ -1,8 +1,47 @@
 <?php
-if(isset($_POST['submitLogin']))
-{
-  $db = new PDO('pgsql:host=database-412.cizjli6h3heh.us-west-2.rds.amazonaws.com;dbname=myDB', 'root', 'videogames');
-  echo "Connected to PostgreSQL";
+$db = new PDO('pgsql:host=database-412.cizjli6h3heh.us-west-2.rds.amazonaws.com;dbname=myDB', 'root', 'videogames');
 
+if (isset($_POST['submitLogin']))
+{
+  try{
+    $query = "SELECT * FROM public.\"Game_Title\"";
+    $db->query($query);
+
+    foreach ($db->query($query) as $row) {
+      print "<br/>";
+      print $row['Game_Title'].'-'.$row['GameID'].'<br/>';
+    }
+  } catch(PDOException $e){
+    echo $e->getMessage();
+  }
+}elseif (isset($_POST['submitAdd']))
+{
+  try{
+    $query = "INSERT INTO public.\"Game_Title\" VALUES('$_POST[vgName]', '$_POST[vgEngine]', '$_POST[vgFranchise]', '$_POST[vgDescription]', '$_POST[vgID]')";
+    $db->query($query);
+    echo "Added";
+  } catch(PDOException $e){
+    echo $e->getMessage();
+  }
+}elseif(isset($_POST['submitDelete'])){
+try{
+  $query = "DELETE FROM public.\"Game_Title\" WHERE public.\"Game_Title\".\"GameID\"='$_POST[vgIDDelete]'";
+  $db->query($query);
+  echo "Deleted";
+}catch(PDOException $e){
+  echo $e->getMessage();
+}
+}elseif (isset($_POST['submitSearch']))
+{
+  try{
+    $query = "SELECT * FROM public.\"Game_Title\" WHERE public.\"Game_Title\".\"GameID\"='$_POST[vgIDSearch]'";
+
+    foreach ($db->query($query) as $row) {
+      print "<br/>";
+      print $row['Game_Title'].'-'.$row['GameID'].'<br/>';
+    }
+  }catch(PDOException $e){
+    echo $e->getMessage();
+  }
 }
 ?>
